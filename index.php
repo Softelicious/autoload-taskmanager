aa34b660d0f0feb49d9377b36aca4a605b06cc33
Sarunas <sarunas.zube@gmail.com>
2019-04-03 12:33:48 +0300

    pabaigiau pas mokytoja

--- index.php
+++ index.php
@@ -2,19 +2,13 @@
 require_once('vendor/autoload.php');
 
 use App\DB;
+use App\QueryBuilder;
 $config = require 'config.php';
 $pdo = DB::connection($config['database']);
 
-$task = "Eiti namo";
-$status = false;
+$builder = new QueryBuilder($pdo);
 
-try {
-/*    $sql = "INSERT INTO tasks (task, status) VALUES (?,?)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$task, $status]);*/
+$builder->read();
+?>
 
-var_dump($pdo->query('SELECT * FROM tasks'));
 
-}catch (PDOException $e){
-    echo $e;
-}
